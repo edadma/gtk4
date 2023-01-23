1ecff11e26c31aafc33f065d79535a9678a24e23
Edward A Maxedon <edadma@gmail.com>
2023-01-22 21:44:55 -0500

    basic example application

--- src/main/scala/io/github/edadma/gtk/extern/LibGTK.scala
+++ src/main/scala/io/github/edadma/gtk/extern/LibGTK.scala
@@ -15,13 +15,30 @@ import scala.scalanative.unsafe._
 @link("glib-2.0")
 @extern
 object LibGTK:
-  type GtkApplication = CStruct0
-  type GtkApplication_p = Ptr[GtkApplication]
-  type GtkWidget = CStruct0
-  type GtkWidget_p = Ptr[GtkWidget]
+//  type GtkWidget = CStruct0
+//  type GtkWidget_p = Ptr[GtkWidget]
+
+  // gtkapplicationwindow.h
+
   type GtkWindow = CStruct0
   type GtkWindow_p = Ptr[GtkWindow]
 
   def gtk_application_window_new(application: GtkApplication_p): GtkWindow_p = extern
+
+  // gtkwindow.h
+
   def gtk_window_set_title(window: GtkWindow_p, title: CString): Unit = extern
+
+  // gtkapplication.h
+
+  type GtkApplication = CStruct0
+  type GtkApplication_p = Ptr[GtkApplication]
+
   def gtk_application_new(application_id: CString, flags: CInt): GtkApplication_p = extern
+
+  // gapplication.h
+
+  type GApplication = GtkApplication
+  type GApplication_p = GtkApplication_p
+
+  def g_application_run(application: GApplication_p, argc: CInt, argv: Ptr[CString]): CInt = extern

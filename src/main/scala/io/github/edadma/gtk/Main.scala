ef972ebd0efbfcfc27fc87fff329e44e25e3c2dc
Edward A Maxedon <edadma@gmail.com>
2023-01-22 21:03:11 -0500

    basic example application

--- src/main/scala/io/github/edadma/gtk/Main.scala
+++ src/main/scala/io/github/edadma/gtk/Main.scala
@@ -1,4 +1,5 @@
 import io.github.edadma.gtk
 
 @main def run(): Unit =
-  val app = gtk.applicationNew("test", )
+  val app = gtk.applicationNew("test", gtk.ApplicationFlags.DEFAULT_FLAGS)
+
--- src/main/scala/io/github/edadma/gtk/package.scala
+++ src/main/scala/io/github/edadma/gtk/package.scala
@@ -4,8 +4,17 @@ import scala.scalanative.unsafe._
 
 import io.github.edadma.gtk.extern.LibGTK as lib
 
-enum ApplicationFlags(bit: Int):
+enum ApplicationFlags(val bit: Int):
+  case DEFAULT_FLAGS extends ApplicationFlags(0)
   case IS_SERVICE extends ApplicationFlags(1 << 0)
-  case IS_SERVICE extends ApplicationFlags(1 << 0)
+  case IS_LAUNCHER extends ApplicationFlags(1 << 1)
+  case HANDLES_OPEN extends ApplicationFlags(1 << 2)
+  case HANDLES_COMMAND_LINE extends ApplicationFlags(1 << 3)
+  case SEND_ENVIRONMENT extends ApplicationFlags(1 << 4)
+  case NON_UNIQUE extends ApplicationFlags(1 << 5)
+  case CAN_OVERRIDE_APP_ID extends ApplicationFlags(1 << 6)
+  case ALLOW_REPLACEMENT extends ApplicationFlags(1 << 7)
+  case REPLACE extends ApplicationFlags(1 << 8)
 
-def applicationNew(application_id: String, flags: Int): Application = extern
+def applicationNew(application_id: String, flags: ApplicationFlags): Application =
+  Zone(implicit z => lib.gtk_application_new(toCString(application_id), flags.bit))

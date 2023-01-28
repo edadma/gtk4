c0543749fe6f234787b6187c6497ec6f053be161
Edward A Maxedon <edadma@gmail.com>
2023-01-28 18:29:47 -0500

    basic example application

--- src/main/scala/io/github/edadma/gtk/Application.scala
+++ src/main/scala/io/github/edadma/gtk/Application.scala
@@ -8,8 +8,7 @@ import scala.scalanative.unsafe.Ptr
 implicit class Application(val ptr: lib.gpointer) extends AnyVal with GObjectTrait:
   def windowNew: Window = lib.gtk_application_window_new(ptr)
 
-  def activate(callback: Application => Unit): Long =
-    super.signalConnect("activate", ptr => callback(ptr))
+  def activate(callback: Application => Unit): Long = super.signalConnect("activate", ptr => callback(ptr))
 
   def run: Int =
     val status = lib.g_application_run(ptr, 0, null)

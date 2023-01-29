324dadf2df208c1202495ec2eee7c55f2741fdb6
Edward A Maxedon Sr <edadma@gmail.com>
2023-01-29 10:02:39 -0500

    Basics example

--- src/main/scala/io/github/edadma/gtk/example/Basics.scala
+++ src/main/scala/io/github/edadma/gtk/example/Basics.scala
@@ -2,7 +2,22 @@ package io.github.edadma.gtk.example
 
 import io.github.edadma.gtk
 
-@main def Basics(): Unit =
+// direct translation of "Basics": https://docs.gtk.org/gtk4/getting_started.html#basics
+@main def Basics1(): Unit =
+  val app = gtk.applicationNew("io.github.edadma.gtk", gtk.ApplicationFlags.DEFAULT_FLAGS)
+
+  app.activate(activate)
+  app.run
+
+def activate(app: gtk.Application): Unit =
+  val window = app.windowNew
+
+  window.setTitle("Window")
+  window.setDefaultSize(200, 200)
+  window.setVisible(true)
+
+// more concise version
+@main def Basics2(): Unit =
   gtk
     .applicationNew("io.github.edadma.gtk", gtk.ApplicationFlags.DEFAULT_FLAGS)
     .activate(

842598b1b22454ae83808023a9e01c5cfc6caf38
Edward A Maxedon <edadma@gmail.com>
2023-01-29 15:11:23 -0500

    HelloWorld example

--- src/main/scala/io/github/edadma/gtk/example/HelloWorld.scala
+++ src/main/scala/io/github/edadma/gtk/example/HelloWorld.scala
@@ -3,7 +3,7 @@ package io.github.edadma.gtk.example
 import io.github.edadma.gtk
 
 // direct translation of "Basics": https://docs.gtk.org/gtk4/getting_started.html#basics
-@main def Basics(): Unit =
+@main def HelloWorld(): Unit =
   def activate(app: gtk.Application): Unit =
     val window = app.windowNew
 
@@ -15,15 +15,3 @@ import io.github.edadma.gtk
 
   app.activate(activate)
   app.run
-
-// more concise version
-@main def Basics2(): Unit =
-  gtk
-    .applicationNew("io.github.edadma.gtk.example", gtk.ApplicationFlags.DEFAULT_FLAGS)
-    .activate(
-      _.windowNew
-        .setTitle("Window")
-        .setDefaultSize(200, 200)
-        .setVisible(true),
-    )
-    .run
